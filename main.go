dce6406bba5017cd2a22e87c22076e6b8dd703e0
Phani Puttabakula <phani.p@bluefunda.com>
2022-11-21 23:36:40 -0800

    apiserver ref

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"syscall"
 
 	// Make sure you change this line to match your module
-	"github.com/phanikumarps/phanikumarps/apiserver"
+	"github.com/phanikumarps/apiserver"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
